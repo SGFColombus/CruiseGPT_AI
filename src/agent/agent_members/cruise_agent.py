7eb217c913069a726215260639eabebd52519ee1
hientq <hien.tranquang@smartgoldfish.ai>
2025-03-21 14:58:46 +0700

    return only selected cabin when add cabin to cart

--- src/agent/agent_members/cruise_agent.py
+++ src/agent/agent_members/cruise_agent.py
@@ -61,7 +61,9 @@ def add_cabin_to_cart(
     state: Annotated[AgentState, InjectedState],
     tool_call_id: Annotated[str, InjectedToolCallId],
 ):
-    """Add a cabin to the cart."""
+    """Add current cabin to the cart."""
+    list_cabins = state.list_cabins
+    list_cabins = [cabin for cabin in list_cabins if cabin["description"] == state.description]
 
     return Command(
         update={
@@ -72,6 +74,7 @@ def add_cabin_to_cart(
                 )
             ],
             "action": "add_cart",
+            "list_cabins": list_cabins,
         }
     )
 
